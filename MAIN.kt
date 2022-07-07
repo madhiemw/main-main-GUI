2df1f719369801d2be89ccfa69223643f96e9af3
MUHAMMAD ADHIEM WICAKSANA <102276858+madhiemw@users.noreply.github.com>
2022-07-08 01:03:52 +0700

    Update MAIN.kt

--- MAIN.kt
+++ MAIN.kt
@@ -1,42 +1,63 @@
-package com.zetcode
 
-import jdk.internal.util.xml.impl.Input
-import java.awt.EventQueue
+import java.awt.Label
 import java.awt.Rectangle
-import java.sql.Struct
+import java.awt.event.FocusListener
 import javax.swing.*
-import java.util.*
+import javax.swing.table.DefaultTableModel
+import javax.swing.text.JTextComponent
 
-fun main (){
-    val myframe = JFrame("DATA MAHASISWA")
-    myframe.setSize(500,500)
-    myframe.defaultCloseOperation=JFrame.EXIT_ON_CLOSE
-    myframe.layout=null
-    val run:Runnable =  Runnable{
-        System.out.println("Muhammad Adhiem Wicaksana ")}
-    System.out.println("UNFINISHED.CO")
-    val t=Thread (run)
-    t.start()
+
+fun main() {
+    // constructor frame 1
+        val myframe = JFrame("LOGIN PAGE")
+    myframe.setSize(500, 500)
+    myframe.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    myframe.layout = null
+    val Input_Nama = JTextField()
+    val Input_Nim = JTextField()
+    val Input_jurusan = JTextField("A11.2019.11897")
     val label = JLabel("PBO")
-    val button= JButton("Submit").apply{
-        actionCommand = "Submit"
-        addActionListener(ButtonClickListener())
-    }
-    val Input_Nama = JTextField("Maasukan Nama")
-    val Input_Nim = JTextField("masukan NIM")
-    val Input_alamat = JTextField("Masukan Alamat")
-    val getfield = Input_Nama.getText()
-    Input_Nim.bounds  =Rectangle(155,220,200,20)
-    Input_alamat.bounds  =Rectangle(155,240,200,20)
-    Input_Nama.bounds  =Rectangle(155,260,200,20)
-    button.bounds= Rectangle(155,300,200,50)
-    label.bounds= Rectangle(240,40,200,100)
+    val button = JButton("Submit")
+    val isisubmit = Input_Nama.getText() //triger dari if else password dan username
+    // constructor frame 2
+    val newframe = JFrame("kandedes")
+    newframe.setSize(500, 500)
+    newframe.layout = null
+    val print_nama = JLabel(Input_Nama.getText())
+    val header_newframe1 = JLabel("UDINUS")
+    val tableModel = DefaultTableModel()
+    val table = JTable(tableModel)
+    tableModel.addColumn("NIM")
+    tableModel.addColumn("NAMA")
+    tableModel.addColumn("jurusan")
+    tableModel.insertRow(0, arrayOf<Any>("MUHAMMAD ADHIEM WICAKSANAN", "A11.2019.11897", "Teknik Informatika"))
+    tableModel.insertRow(1, arrayOf<Any>(Input_Nama.getText()))
+    // bounds posisi setiap variable frame 1
+    Input_Nim.bounds = Rectangle(155, 220, 200, 20)
+    Input_jurusan.bounds = Rectangle(155, 240, 200, 20)
+    Input_Nama.bounds = Rectangle(155, 260, 200, 20)
+    button.bounds = Rectangle(155, 300, 200, 50)
+    label.bounds = Rectangle(240, 40, 200, 100)
+    //bounds posisi setiap variable frame 2
+    table.bounds = Rectangle(0, 100, 500, 500)
+    header_newframe1.bounds = Rectangle(200, 20, 200, 100)
+    print_nama.bounds = Rectangle(240, 40, 200, 100)
+    if (isisubmit.equals("A11.2019.11897")) {
+            button.addActionListener {
+            newframe.add(header_newframe1)
+            newframe.add(print_nama)
+            newframe.add(table)
+            newframe.isVisible = true
+        }
+}
+    //add frame 1
     myframe.add(button)
     myframe.add(Input_Nama)
     myframe.add(label)
     myframe.add(Input_Nim)
-    myframe.add(Input_alamat)
+    myframe.add(Input_jurusan)
     myframe.isVisible=true
-
-
 }
+
+
+
